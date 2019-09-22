5cae8b0c392fb1556ef2d582b0dc272e118b74b4
Anna-Petko <annapetko@icloud.com>
2019-09-22 22:19:10 +0300

    Added database schema for organization and values (data.sql)

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS Organization(
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(100) NOT NULL,
+	inn VARCHAR(100) NOT NULL,
+	is_active TINYINT default 1,
+	full_name VARCHAR(100) NOT NULL,
+	kpp VARCHAR(100) NOT NULL,
+	address VARCHAR(100) NOT NULL,
+	phone VARCHAR(100)
+);
+COMMENT ON TABLE Organization IS 'Организация';
