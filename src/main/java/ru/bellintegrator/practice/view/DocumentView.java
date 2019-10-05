e1418e9421c9e0fc13f67bff0163fb96076ca6f0
Anna-Petko <annapetko@icloud.com>
2019-10-05 20:56:37 +0300

    Added views (success and error messages, + + view for document)

---
+++ src/main/java/ru/bellintegrator/practice/view/DocumentView.java
@@ -0,0 +1,27 @@
+package ru.bellintegrator.practice.view;
+
+public class DocumentView {
+	public String name;
+	public String code;
+
+	public DocumentView() {
+
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+
+}
